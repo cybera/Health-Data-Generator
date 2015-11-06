b27ff1efc1d4026e024f015ae3fdd9f03cb50af9
Barton Satchwill <barton.satchwill@cybera.ca>
2015-11-06 12:29:55 -0700

    generate multiple 'medical records' for each patient

--- fake-data.py
+++ fake-data.py
@@ -7,7 +7,7 @@ import random, string, sys, collections
 header = None
 record_count = 0
 
-def generate_simple_record():
+def generate_simple_patient():
 	od = collections.OrderedDict()
 	od['phn'] = demographics.ID()
 	od['first_name'] = name.first_name()
@@ -16,6 +16,19 @@ def generate_simple_record():
 	od['city'] = address.ca_city()
 	od['postal_code'] = address.ca_postal_code(od['city'])
 	od['education'] = demographics.education_code()
+	return od
+
+
+def generate_simple_medical_record():
+	od = collections.OrderedDict()
+	# od['phn'] = demographics.ID()
+	# od['first_name'] = name.first_name()
+	# od['last_name'] = name.last_name()
+	# od['birthdate'] = demographics.birthdate()
+	# od['city'] = address.ca_city()
+	# od['postal_code'] = address.ca_postal_code(od['city'])
+	# od['education'] = demographics.education_code()
+
 	od['systolic'] = clinical.systolic()
 	od['diastolic'] = clinical.diastolic(od['systolic'])
 	od['ICD'] = clinical.ICD_code()
@@ -24,7 +37,7 @@ def generate_simple_record():
 	return od
 
 
-def generate_tricky_record():
+def generate_complex_patient():
 	od = collections.OrderedDict()
 	od['phn'] = demographics.ID()
 	od['name'] = name.find_name()
@@ -35,6 +48,11 @@ def generate_tricky_record():
 	od['gender'] = demographics.gender()
 	od['phone'] = phone_number.phone_number()
 	od['education'] = '"' + demographics.education() + '"'
+	return od
+
+
+def generate_complex_medical_record():
+	od = collections.OrderedDict()
 	od['blood_pressure'] = clinical.blood_pressure()
 	od['ICD'] = '"' + clinical.ICD() + '"'
 	od['glycohemoglobin'] = clinical.glycohemoglobin()
@@ -49,21 +67,35 @@ def print_record(record):
 
 def write_record(record, file):
 	global header
-	values = ''
 
 	if not header:
-		header = True
 		f = open(file, 'w')
 		header = ', '.join(record.keys()).upper()
 		f.write(header + '\n')
 		f.close()
+		header = True
 
-	for key, value in record.iteritems():
-		values += str(value) + ', '
-	values = values[:-2]
 	f = open(file, 'a')
-	f.write(values + '\n')
+	row = ', '.join(map(str, record.values()))
+	f.write(row +'\n')
 	f.close()
+
+
+def generate_patient(mode):
+	if mode == 'simple':
+		return generate_simple_patient()
+	if mode == 'complex':
+		return generate_complex_patient()
+	return None
+
+def generate_medical_record(mode):
+	if mode == 'simple':
+		return generate_simple_medical_record()
+	if mode == 'complex':
+		return generate_complex_medical_record()
+	return None
+
+
 
 
 if __name__ == '__main__':
@@ -75,22 +107,27 @@ if __name__ == '__main__':
 		count = int(sys.argv.pop())
 
 	if mode == 'test':
-		r = generate_tricky_record()
-		print_record(r)
+		p = generate_simple_patient()
+		print_record(p)
+		for i in range (0, random.randint(0,9)):
+			r = generate_simple_medical_record()
+			for k,v in r.iteritems():
+				p[k]=v
+			print "-------------"
+			print_record(r)
+			exit()
 
-	if mode == 'simple':
-		print "generating " + str(count) + " " + mode + " records"
-		for i in range(0,count):
-			r = generate_simple_record()
-			write_record(r, '../fake_data_simple.csv')
-			print i+1
-
-	if mode == 'tricky':
-		print "generating " + str(count) + " " + mode + " records"
-		for i in range(0, count):
-			r = generate_tricky_record()
-			write_record(r, '../fake_data_complex.csv')
-			print i+1
-
-
-	
+	i = 0
+	print "generating " + str(count) + " " + mode + " records"
+	while i < count:
+		p = generate_patient(mode)
+		j = random.randint(1,9)
+		while i < count and j > 0:
+			r = generate_medical_record(mode)
+			# append medical record to patient record
+			for k,v in r.iteritems():
+				p[k]=v
+			write_record(p, '../fake_data_%s.csv' % mode)
+			i+=1
+			j-=1
+		print i
